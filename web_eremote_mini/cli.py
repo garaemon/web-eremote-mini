f1293644f4c20aff663432245b3a16f8efb3a77e
Ryohei Ueda <garaemon@gmail.com>
2017-12-31 16:11:22 +0900

    Change default database directory to project local directory

--- web_eremote_mini/cli.py
+++ web_eremote_mini/cli.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
+import os
+
 import fire
 
 from .server import Server
 
+PROJECT_DIR = os.path.join(os.path.dirname(__file__), '..')
+
 class CliApp(object):
     'Application class for cli usage'
-    def serve(self, port=8880):
+    def serve(self, port=8880, database_directory=os.path.join(PROJECT_DIR, 'db')):
         'Run server'
-        s = Server(port=port)
+        s = Server(port=port, db_dir=database_directory)
         s.run()
 
 
